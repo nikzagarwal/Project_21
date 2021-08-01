6cc8361d19f824dbaf77d3b8c5f98206a7fc608c
PaarthBarkur1 <paarthbarkur@gmail.com>
2021-08-01 22:02:10 +0530

    metrics changes made

--- Files/metrics.py
+++ Files/metrics.py
@@ -1,6 +1,6 @@
 from numpy.core.numeric import NaN
 from sklearn.metrics import accuracy_score , recall_score, precision_score, f1_score, cohen_kappa_score, matthews_corrcoef
-from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,mean_squared_log_error
+from sklearn.metrics import mean_absolute_error,auc,mean_squared_error,r2_score,mean_squared_log_error
 
 
 class Metrics:
@@ -9,17 +9,21 @@ class Metrics:
 
 
         if model_type=="Classification":
-            criterias=["accuracy_score" , "recall_score", "precision_score", "f1_score", "cohen_kappa_score", "matthews_corrcoef"]
+            criterias=["accuracy_score","recall_score","precision_score","f1_score","cohen_kappa_score","matthews_corrcoef"]
         #metrics.loc[len(metrics.index)]=[modelname,accuracy_score(y,prediction),recall_score(y,prediction),precision_score(y,prediction),f1_score(y,prediction),cohen_kappa_score(y,prediction),matthews_corrcoef(y,prediction)]
 
         elif model_type=="Regression":
-            criterias=["mean_absolute_error","mean_squared_error","r2_score","mean_squared_log_error"]
+            criterias=["mean_absolute_error","mean_squared_error","r2_score","mean_squared_log_error","auc"]
 
         metricsnewrow=[modelname]
         for criteria in criterias:
             try:
-                metricsnewrow.append(round(eval(criteria+"(y,prediction)"),2))
-
+                if criteria != "mean_squared_error":
+                    metricsnewrow.append(round(eval(criteria+"(y,prediction)"),2))
+                else:
+                    mse=round(eval(criteria+"(y,prediction)"),2)
+                    metricsnewrow.append(mse)
+                    metricsnewrow.append(mse**(1/2))
             except Exception as e:
                 print(e)
                 if model_type=="Classification":
