88f48014e2ee32feee51fad27a6473dbc68c544a
Nikhil Agarwal <nikhilagarwalassisi@gmail.com>
2021-08-15 12:59:34 +0530

    solved manual models api hyperparam key issue

--- Frontend/pr21/src/components/section1.js
+++ Frontend/pr21/src/components/section1.js
@@ -7,7 +7,7 @@ class Section1 extends Component {
                 <div className="container typing-text">
                     <p>Curl Brings <span className="typed-text"></span><span className="cursor">&nbsp;</span></p>
                     <p>Together under one Umbrella</p>
-                    <div className="section1text1">The Easy to go auto-ml engine for all your data, it creates end to end experience of machine and deep learning without a single line of code</div>
+                    <div className="section1text1">Auto Ml is latest and advanced way to simplify artificial intelligence usage for people who are not into coding but still want to experience the magic</div>
                     <div className="sec1btn-group">
                         <a href='#section2'> <button className="  btn  section1button section1button2">Start Expereince </button></a>
                         <a href='#section3'> <button className=" btn btn-primary section1button section1button2 ">View Demo </button></a>
